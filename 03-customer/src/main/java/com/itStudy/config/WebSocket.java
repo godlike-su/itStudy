f5b3556615cdc65e44f28ff16d19f4a23583d098
godlike-su <1477719800@qq.com>
2021-04-25 21:33:44 +0800

    first commit

--- 03-customer/src/main/java/com/itStudy/config/WebSocket.java
+++ 03-customer/src/main/java/com/itStudy/config/WebSocket.java
@@ -4,17 +4,17 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * 全双工通信， WebSocket
  * 打包成war包需要注释掉
  */
-//@Configuration
+@Configuration
 public class WebSocket
 {
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter(){
-//        return new ServerEndpointExporter();
-//    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
 }
